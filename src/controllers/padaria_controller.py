e7600e92da5ec15bcd728cb7b47fa0bfdad6315a
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-06-25 23:40:12 -0300

    feat: create a class PadariaController in padaria_controller whit entities view, fornecedor_,model, produto_model.

--- src/controllers/padaria_controller.py
+++ src/controllers/padaria_controller.py
@@ -1,3 +1,9 @@
 from models.fornecedor import Fornecedor, FornecedorModel
 from models.produto import Produto, ProdutoModel
 from views.padaria_view import PadariaView
+
+class PadariaController:
+    def __init__(self):
+        self.view = PadariaView()
+        self.fornecedor_model = FornecedorModel()
+        self.produto_model = ProdutoModel()
