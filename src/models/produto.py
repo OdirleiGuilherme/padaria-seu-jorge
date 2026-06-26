da8589e52dd6118e83315ca8bcf4bd9c6ffe1741
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-06-26 00:45:29 -0300

    feat: add gerardor de IDs automatcos para produtos usando contador interno para evitar duplicidade de IDs caso seja deletado algum produto.

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,6 +1,6 @@
 class Produto:
     def __init__(self, id_produto, nome_produto, preco_produto, id_fornecedor):
-        self.id = id_produto
+        self.id = None
         self.nome = nome_produto
         self.preco = preco_produto
         self.id_fornecedor = id_fornecedor # adicionando o atributo id_fornecedor para associar o produto a um fornecedor
@@ -8,9 +8,13 @@ class Produto:
 class ProdutoModel:
     def __init__(self):
         self.produtos = []
-        
+        self.proxima_id = 1  # Inicializa o próximo ID como 1
+    
     def salvar(self, produto: Produto):
+        produto.id = self.proxima_id # atribui o ID auomaticamente no objeto Produto
         self.produtos.append(produto)
+        self.proxima_id += 1 # incrementa o próximo ID para o próximo produto
+        return produto.id # retorna o ID do produto cadastrado
     
     def listar_produto(self):
         return self.produtos
